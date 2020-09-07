c5766b25b623848f34502d958b71df7dbf37b6d3
MolarFox <rrjayaram@outlook.com.au>
2020-09-08 00:13:00 +1000

    Setup google maps / 'AGM' npm package integration

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { GMAPS_API_KEY } from './gmaps_apikey';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { AgmCoreModule } from '@agm/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { MainmenuComponent } from './mainmenu/mainmenu.component';
 import { ViewRoutesComponent } from './view-routes/view-routes.component';
 import { ViewHeatmapComponent } from './view-heatmap/view-heatmap.component';
@@ -28,7 +31,10 @@ import { ViewFleetComponent } from './view-fleet/view-fleet.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    AgmCoreModule.forRoot({
+      apiKey: GMAPS_API_KEY
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
