d0e571f9f81cf811c29719726630182396afcf92
MolarFox <rrjayaram@outlook.com.au>
2020-09-09 09:44:47 +1000

    Fix bugs by rolling back AGM version to 1.1.0
    WHY DOES IT JUMP TO VERSION 3 BETA WHY WOULD YOU DO THIS AAAAAAAA
    
    WEB DEV IS DESCUSTANG

--- Frontend/src/app/view-bins/view-bins.component.ts
+++ Frontend/src/app/view-bins/view-bins.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AgmMarker } from '@agm/core';
 
 @Component({
   selector: 'app-view-bins',
@@ -6,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-bins.component.css']
 })
 export class ViewBinsComponent implements OnInit {
+
+  start_lat = -37.8142588;
+  start_lng = 144.9666622;
+  start_zoom = 14;
+
+  // TEMP CODE TO GET DUMMY BIN LOCS
+  markers: any[] = [];
+
+  mapClicked($event: any) {
+    console.log($event)
+    this.markers.push({
+      lat:   $event.coords.lat,
+      lng:  $event.coords.lng,
+      draggable: true
+    });
+  }
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`)
+  }
 
   constructor() { }
 
