684e3c1757bcf782a4578b6ff9f6d8129f08c18f
MolarFox <MolarFox@outlook.com>
2020-10-18 10:45:10 +0000

    add console log for route origin on load

--- Frontend/src/app/view-routes/view-routes.component.ts
+++ Frontend/src/app/view-routes/view-routes.component.ts
@@ -1,5 +1,6 @@
 import { GoogleMapsAPIWrapper } from '@agm/core';
 import { Component, OnInit } from '@angular/core';
+import { RoutefetcherService } from '../routefetcher.service'
 import { GoogleMap } from '@angular/google-maps';
 
 @Component({
@@ -13,24 +14,27 @@ export class ViewRoutesComponent implements OnInit {
   start_lng = 144.9666622;
   start_zoom = 14;
 
-  origin: any = {
-    latitude: -37.907803,
-    longitude: 145.133957
+  origin: google.maps.LatLngLiteral = {
+    lat: -37.907803,
+    lng: 145.133957
   };
 
-  destination: any = {
-    latitude: -37.8997609,
-    longitude: 145.1292176
+  destination: google.maps.LatLngLiteral = {
+    lat: -37.8997609,
+    lng: 145.1292176
   };
 
-  waypoints: any = []
+  waypoints: google.maps.DirectionsWaypoint[] = [
+    
+  ]
 
   displayDirections = true;
   zoom = 14; 
 
-  constructor() { }
+  constructor(private routefetcher: RoutefetcherService) { }
 
   ngOnInit(): void {
+    console.log(this.origin)
   }
 
 }
